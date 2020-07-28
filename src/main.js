c0cef235b24e292583b9f06f9712a08a78c81bfa
timibadass <omoyenitimi@gmail.com>
2020-07-28 23:41:14 +0100

    set up vue loading overlay for use as a component

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+Vue.component('loading-overlay', Loading)
+
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
